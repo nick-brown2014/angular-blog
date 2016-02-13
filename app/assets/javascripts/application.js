2b011d2ac503f3ae7be8911d63bdeb4c03ac5590
Nick Brown <nick.brown2014@gmail.com>
2016-02-13 10:42:38 -0700

    added contact section, made menu scroll

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,47 @@
 //= require turbolinks
 //= require angular
 //= require_tree .
+
+// angular.module("myBlog", [])
+// .controller("main", function() {
+
+// });
+
+$(document).ready(function() {
+
+  $("#about-scroll").click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: ($("#about").offset().top - 50)
+    }, 1000);
+  });
+
+  $("#projects-scroll").click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: ($("#projects").offset().top - 49)
+    }, 1000);
+  });
+
+  $("#stories-scroll").click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: ($("#stories").offset().top - 70)
+    }, 1000);
+  });
+
+  $("#contact-scroll").click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: ($("#contact").offset().top - 50)
+    }, 1000);
+  });
+
+  $("#top-scroll").click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: ($(".container").offset().top - 50)
+    }, 1000);
+  });
+
+});
